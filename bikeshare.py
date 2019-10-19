384f0e107615255c8fb987c9dedbac16d02f2a40
Varun Gulati <varungulati.1985@gmail.com>
2019-10-19 17:36:31 +0530

    More changes on main function while loop Comments

--- bikeshare.py
+++ bikeshare.py
@@ -189,6 +189,7 @@ def main():
         user_stats(df)
         raw_data(df)
         
+        #while loop for looping through dataset
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
